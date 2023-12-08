e59476b7d3f76daeaf09228ac75dd974f69c2797
Mg Chit <chitlunh@gmail.com>
2023-12-09 01:12:34 +0630

    Add project files.

---
+++ BlazorServer.ServiceLifeTime/Services/ScopedService.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorServer.ServiceLifeTime.Services
+{
+    public class ScopedService
+    {
+        public Guid Guid { get; set; }
+
+        public ScopedService()
+          => Guid = Guid.NewGuid();
+    }
+    public class SingletonService
+    {
+        public Guid Guid { get; set; }
+
+        public SingletonService()
+          => Guid = Guid.NewGuid();
+    }
+    public class TransientService
+    {
+        public Guid Guid { get; set; }
+
+        public TransientService()
+          => Guid = Guid.NewGuid();
+    }
+}
